97820829e96a31d59692aef10044a00c01960f94
Pouya Jamali <pouyajamali@gmail.com>
2022-07-19 14:25:10 +0430

    first draft commit

--- esmfamil_flaskapi.py
+++ esmfamil_flaskapi.py
@@ -3,32 +3,24 @@ import json
 
 app = Flask(__name__)
 
-# Endpoint01 - participants
+# Endpoint01 - register
+@app.route('/register', methods=['POST'])
+def templates():
+    if request.method=='POST':
+        pass
+
+# Endpoint02 - submit
 @app.route('/participants', methods=['POST'])
 def templates():
     if request.method=='POST':
+        pass
     
 
-
-# Endpoint02 - events
-@app.route('/events', methods=['GET', 'POST'])
+# Endpoint03 - results
+@app.route('/results', methods=['GET', 'POST'])
 def events():
     if request.method=='GET':
-        print('this is a get request')
-        print(request.args)
-        return jsonify({'method': 'GET', 'result': request.args})
-    elif request.method=='POST':
-        print('this is a post request')
-        return {'method': 'POST', 'result': request.args}
-    else:
         pass
-
-# Endpoint03 - hosts
-# Endpoint04 - guests
-
-
-
-
 
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a Repo created for test purposes. 
+
+Rahnema College will use the codes here to evaluate candidates.
